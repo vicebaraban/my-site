ed7f68cf11d3e9d73b7f27a0ed3871f933c68d94
Avgust Varlamov <varl.avgust@yandex.ru>
2024-12-23 18:35:47 +0300

    add first vizitka version

--- run_site.py
+++ run_site.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
