bcb9e60f7b20d72d3d0e982b686451e8bc229f11
Avgust Varlamov <varl.avgust@yandex.ru>
2025-04-07 00:12:29 +0300

    add config

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, request, session, jsonify
+import configparser
 
 
 app = Flask(__name__, static_url_path='',
             static_folder="static", template_folder="templates")
+
+config = configparser.ConfigParser()
+try:
+    config.read('config.conf')
+except Exception:
+    raise FileNotFoundError
+
+app.secret_key = config['settings']['secret_key']
 
 
 @app.route("/vk_ok_auth", methods=["POST"])
